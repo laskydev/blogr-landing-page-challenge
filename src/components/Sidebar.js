6fc826ad68f9866584b9a79f3f1805697ffd1c1d
laskydev <eduardofortuna99@gmail.com>
2021-03-28 23:05:38 -0600

    Avance menu sidebar

---
+++ src/components/Sidebar.js
@@ -0,0 +1,146 @@
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+import { colors } from "../design/variables";
+import iconArrow from '../images/icon-arrow-dark.svg'
+const Container = styled.div`
+    background-color: white;
+    width: 90vw;
+    min-height: 60vh;
+    z-index: 1;
+    margin: 0 auto;
+    border-radius: .5rem;
+    /* Sombra */
+    -webkit-box-shadow: 8px 8px 10px -2px rgba(0,0,0,0.29); 
+    box-shadow: 8px 8px 10px -2px rgba(0,0,0,0.29);
+        ul{
+            margin:0;
+            padding: 1.5rem;
+            display: flex;
+            flex-direction: column;
+            list-style: none;
+            align-items: center;
+
+                span{
+                    background-color: ${colors.neutral.grayshBlue};
+                    height: 1px;
+                    width: 80%;
+                    margin-top: 2rem;
+                    margin-bottom: 2rem;
+                }
+
+                li{
+                    font-size: 1.3rem;
+                    font-weight: 600;
+                    cursor: pointer;
+                    padding: 1rem;
+                    position: relative;
+                        span{
+                            background-image: url(${iconArrow});
+                            background-color: transparent;
+                            width: 10px;
+                            height: 7px;
+                            position: absolute;
+                            top: 50%;
+                            transform: translate(0, -50%);
+                            margin: 0 0 0 .5rem;
+                        }
+                }
+
+                button{
+                    border: none;
+                    border-radius: 1.5rem;
+                    padding: 1rem 3rem;
+                    background-image:
+                        linear-gradient(
+                            90deg,
+                            rgba(255, 143, 112, 1) 0%,
+                            rgba(255, 61, 84, 1) 100%
+                        );
+                    color: ${colors.neutral.white};
+                    font-weight: 600;
+                    margin-top: .5rem;
+                }
+        }
+
+`
+const SubMenu = styled.div`
+    width: 100%;
+    border-radius: .5rem;
+    background-color: #ececec;
+    padding: 1rem;
+
+        p{
+            text-align: center;
+            font-size: 1.1rem;
+            padding: .5rem 0;
+            font-weight: 600;
+            color: ${colors.neutral.veryDarkGrayshBlue};
+            cursor: pointer;
+        }
+`
+
+const Sidebar = () => {
+
+    const [subMenuClick, setSubMenuClick] = useState({
+        product: false,
+        company: false,
+        connect: true
+    });
+
+    const resetSubmenus = () => setSubMenuClick({product: false, company: false, connect: false,})
+    const subMenuClicked = e =>{
+        
+        switch (e.target.textContent.trim()){
+            case('Product'):
+            resetSubmenus()
+                setSubMenuClick({...subMenuClick, product:true})
+                break;
+            case('Company'):
+            resetSubmenus()
+                setSubMenuClick(...subMenuClick,{company:true})
+                break;
+            case('Connect'):
+            resetSubmenus()
+                setSubMenuClick(...subMenuClick,{connect:true})
+                break;
+            default:
+                break;
+        }
+
+    }
+    return ( 
+        <Container>
+            <ul>
+                <li onClick={subMenuClicked}>Product <span></span></li>
+                {subMenuClick.product
+                ? <SubMenu>
+                    <p>Contact</p>
+                    <p>Newsletter</p>
+                    <p>Linkedin</p>
+                </SubMenu> 
+                : null}
+                <li onClick={subMenuClicked}>Company <span></span></li>
+                {subMenuClick.company
+                ? <SubMenu>
+                    <p>Contact</p>
+                    <p>Newsletter</p>
+                    <p>Linkedin</p>
+                </SubMenu> 
+                : null}
+                <li onClick={subMenuClicked}>Connect <span></span></li>
+                {subMenuClick.connect
+                ? <SubMenu>
+                    <p>Contact</p>
+                    <p>Newsletter</p>
+                    <p>Linkedin</p>
+                </SubMenu> 
+                : null}
+                <span></span>
+                <li>Login </li>
+                <button type="button">Sign Up</button>
+            </ul>
+        </Container>
+    );
+}
+
+export default Sidebar;
